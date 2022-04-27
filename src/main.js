ed493dc815802bd7e6e600ffcd525945028256dd
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-04-27 15:38:14 +0200

    Example with global mixin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import loggerMixin from './mixins/logger.js';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(store).use(router);
+
+// Global mixin example
+app.mixin(loggerMixin);
+
+app.mount('#app');
