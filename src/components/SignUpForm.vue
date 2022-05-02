8d3dd0b6aa0ae38633c450fd0318a8c09ed487cc
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-05-02 14:21:55 +0200

    Same password validation.

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -4,54 +4,48 @@
       <label for="name" class="form-label">Name</label>
       <input
         v-focus
-        v-model="validationModel.name.$model"
+        v-model="v$.name.$model"
         type="text"
         name="name"
         id="name"
         class="form-control"
       />
-      <ValidationMessage :model="validationModel.name" class="pt-2" />
+      <ValidationMessage :model="v$.name" class="pt-2" />
     </div>
     <div class="form-group mb-2">
       <label for="email" class="form-label">Email</label>
       <input
-        v-model="validationModel.email.$model"
+        v-model="v$.email.$model"
         id="email"
         type="email"
         class="form-control"
       />
-      <ValidationMessage :model="validationModel.email" class="pt-2" />
+      <ValidationMessage :model="v$.email" class="pt-2" />
     </div>
     <div class="form-group mb-2">
       <label for="password" class="form-label">Password</label>
       <input
-        v-model="validationModel.password.$model"
+        v-model="v$.password.password.$model"
         type="password"
         name="password"
         id="password"
         class="form-control"
       />
-      <ValidationMessage :model="validationModel.password" class="pt-2" />
+      <ValidationMessage :model="v$.password.password" class="pt-2" />
     </div>
     <div class="form-group mb-2">
       <label for="confirmPassword" class="form-label">Confirm Password</label>
       <input
-        v-model="validationModel.confirmPassword.$model"
+        v-model="v$.password.confirm.$model"
         type="password"
         name="confirmPassword"
         id="confirmPassword"
         class="form-control"
       />
-      <ValidationMessage
-        :model="validationModel.confirmPassword"
-        class="pt-2"
-      />
+      <ValidationMessage :model="v$.password.confirm" class="pt-2" />
     </div>
     <div class="text-end">
-      <button
-        class="btn btn-outline-primary"
-        :disabled="validationModel.$invalid"
-      >
+      <button class="btn btn-outline-primary" :disabled="v$.$invalid">
         Register
       </button>
     </div>
@@ -60,11 +54,12 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
-import signUpValidation from "@/models/validation/SignUpModel.js";
 import ValidationMessage from "@/components/ValidationMessage";
+import { useVuelidate } from "@vuelidate/core";
+import { required, minLength, email, sameAs } from "@vuelidate/validators";
 export default {
   name: "SignUpForm",
   components: {
@@ -80,18 +75,38 @@ export default {
     const store = useStore();
     const router = useRouter();
 
-    const validationModel = signUpValidation.toModel();
+    const state = reactive({
+      name: "",
+      email: "",
+      password: {
+        password: "",
+        confirm: "",
+      },
+    });
+
+    const rules = computed(() => {
+      return {
+        name: { required, minLength: minLength(2) },
+        email: { required, email },
+        password: {
+          password: { required, minLength: minLength(8) },
+          confirm: { required, sameAs: sameAs(state.password.password) },
+        },
+      };
+    });
+
+    const v$ = useVuelidate(rules, state);
     const errorMessage = ref(null);
 
     async function handleSubmit() {
       try {
-        const valid = await validationModel.value.$validate();
+        const valid = await v$.value.$validate();
         if (!valid) return;
 
         await store.dispatch(props.actionName, {
-          name: validationModel.value.name.$model,
-          email: validationModel.value.email.$model,
-          password: validationModel.value.password.$model,
+          name: v$.value.name.$model,
+          email: v$.value.email.$model,
+          password: v$.value.password.password.$model,
         });
         router.push("/");
       } catch (err) {
@@ -101,7 +116,7 @@ export default {
     }
 
     return {
-      validationModel,
+      v$,
       handleSubmit,
       errorMessage,
     };
